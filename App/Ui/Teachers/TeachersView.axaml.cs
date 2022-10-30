a32ff23ca143115401063d146760e01b4bdf42f9
danilka108 <danilka108@yandex.ru>
2022-10-30 18:20:17 +0700

    add mapper from DayOfWeek to string

--- App/Ui/Teachers/TeachersView.axaml.cs
+++ App/Ui/Teachers/TeachersView.axaml.cs
@@ -1,14 +1,54 @@
-using App.ViewModels;
+using System;
+using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
 
 namespace App.Ui.Teachers;
 
-public class Teachers : UserControl
+// ReSharper disable once PartialTypeWithSinglePart
+public partial class TeachersView : UserControl
 {
-    public Teachers()
+    private const string MondayResourceKey = "MondayString";
+    private const string TuesdayResourceKey = "TuesdayString";
+    private const string WednesdayResourceKey = "WednesdayString";
+    private const string ThursdayResourceKey = "ThursdayString";
+    private const string FridayResourceKey = "FridayString";
+    private const string SaturdayResourceKey = "SaturdayString";
+    private const string SundayResourceKey = "SundayString";
+
+    public TeachersView()
     {
+        DataContext = new TeachersViewModel(mapDayOfWeekToString);
         AvaloniaXamlLoader.Load(this);
-        DataContext = new TeachersViewModel();
+    }
+
+    private string mapDayOfWeekToString(DayOfWeek dayOfWeek)
+    {
+        var key = dayOfWeek switch
+        {
+            DayOfWeek.Monday => MondayResourceKey,
+            DayOfWeek.Tuesday => TuesdayResourceKey,
+            DayOfWeek.Wednesday => WednesdayResourceKey,
+            DayOfWeek.Thursday => ThursdayResourceKey,
+            DayOfWeek.Friday => FridayResourceKey,
+            DayOfWeek.Saturday => SaturdayResourceKey,
+            DayOfWeek.Sunday => SundayResourceKey
+        };
+
+        object? value = null;
+        var isFound = Application.Current?.TryFindResource(key, out value);
+
+        if (isFound != true) throw new MapDayOfWeekToStringException($"Failed to found resource '{key}'");
+
+        if (value is string v) return v;
+
+        throw new MapDayOfWeekToStringException("Resource with 'string' type is expected");
+    }
+
+    public class MapDayOfWeekToStringException : Exception
+    {
+        public MapDayOfWeekToStringException(string msg) : base(msg)
+        {
+        }
     }
 }
--- App/Ui/Teachers/TeachersViewModel.cs
+++ App/Ui/Teachers/TeachersViewModel.cs
@@ -1,21 +1,67 @@
+using System;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Linq;
 using Domain.Models;
+using Teacher = App.Models.Teacher;
 
-namespace App.ViewModels;
+namespace App.Ui.Teachers;
 
 public class TeachersViewModel : ViewModelBase
 {
-    private readonly ClassTimeBounds[] _classesTileBounds =
+    private static readonly ClassTimeBounds[] ClassesTileBounds =
     {
-        new(1, new BellTime(8, 30), new BellTime(10, 00)),
-        new(2, new BellTime(10, 15), new BellTime(11, 45)),
-        new(3, new BellTime(12, 00), new BellTime(13, 30)),
-        new(4, new BellTime(14, 00), new BellTime(15, 30)),
-        new(5, new BellTime(15, 45), new BellTime(17, 15)),
-        new(6, new BellTime(17, 30), new BellTime(19, 00)),
-        new(7, new BellTime(19, 15), new BellTime(20, 45)),
-        new(8, new BellTime(21, 00), new BellTime(22, 30))
+        new(new BellTime(8, 30), new BellTime(10, 00)),
+        new(new BellTime(10, 15), new BellTime(11, 45)),
+        new(new BellTime(12, 00), new BellTime(13, 30)),
+        new(new BellTime(14, 00), new BellTime(15, 30)),
+        new(new BellTime(15, 45), new BellTime(17, 15)),
+        new(new BellTime(17, 30), new BellTime(19, 00)),
+        new(new BellTime(19, 15), new BellTime(20, 45)),
+        new(new BellTime(21, 00), new BellTime(22, 30))
     };
 
-    public ObservableCollection<Teacher> Items { get; }
+    public TeachersViewModel(Func<DayOfWeek, string> dayOfWeekToStringMapper)
+    {
+        Teachers = new ObservableCollection<Teacher>(CreateTestTeachers(dayOfWeekToStringMapper));
+    }
+
+    public ObservableCollection<Teacher> Teachers { get; }
+
+    private static IEnumerable<Teacher> CreateTestTeachers(Func<DayOfWeek, string> dayOfWeekToStringMapper)
+    {
+        var timeBounds1 = ClassesTileBounds.Where((_, i) => i % 2 == 0).ToList();
+        var timeBounds2 = ClassesTileBounds.Where((_, i) => i % 2 != 0).ToList();
+
+        var teacher1 = new Teacher("Попов", "Евгений", "Александрович", new Models.Schedule(
+            dayOfWeekToStringMapper,
+            new Dictionary<DayOfWeek, Domain.Models.DailySchedule>
+            {
+                { DayOfWeek.Monday, new Domain.Models.DailySchedule(timeBounds1) },
+                { DayOfWeek.Tuesday, new Domain.Models.DailySchedule(timeBounds2) },
+                { DayOfWeek.Wednesday, new Domain.Models.DailySchedule(timeBounds1) },
+                { DayOfWeek.Thursday, new Domain.Models.DailySchedule(timeBounds2) },
+                { DayOfWeek.Friday, new Domain.Models.DailySchedule(timeBounds1) },
+                { DayOfWeek.Saturday, new Domain.Models.DailySchedule(timeBounds2) },
+                { DayOfWeek.Sunday, new Domain.Models.DailySchedule(timeBounds1) }
+            }));
+
+        var teacher2 = new Teacher("Левяков", "Станислав", "Вячеславович", new Models.Schedule(
+            dayOfWeekToStringMapper,
+            new Dictionary<DayOfWeek, Domain.Models.DailySchedule>
+            {
+                { DayOfWeek.Monday, new Domain.Models.DailySchedule(timeBounds2) },
+                { DayOfWeek.Tuesday, new Domain.Models.DailySchedule(timeBounds1) },
+                { DayOfWeek.Wednesday, new Domain.Models.DailySchedule(timeBounds2) },
+                { DayOfWeek.Thursday, new Domain.Models.DailySchedule(timeBounds1) },
+                { DayOfWeek.Friday, new Domain.Models.DailySchedule(timeBounds2) },
+                { DayOfWeek.Saturday, new Domain.Models.DailySchedule(timeBounds1) },
+                { DayOfWeek.Sunday, new Domain.Models.DailySchedule(timeBounds2) }
+            }));
+
+        return new[]
+        {
+            teacher1, teacher2
+        };
+    }
 }
